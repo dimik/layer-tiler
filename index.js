19ee265568edb6591b5ff4d1c18214b56d5c965f
dimik <dimik@yandex.ru>
2013-08-23 22:53:02 +0700

    add benchmark code

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var Tiler = require('./lib/layer-tiler.js'),
-    destination = __dirname + '/images';
+    destination = __dirname + '/images',
+    start = new Date;
 
 var tiler = new Tiler({ output: destination });
 
+// tiler.openSource(destination + '/cosmo_map_final.png')
 tiler.openSource(destination + '/big.jpg')
     .then(function () {
-        tiler.render();
+        tiler.render()
+            .then(function () {
+                console.log('Rendered in %ds', Math.ceil((new Date - start) / 1000));
+            });
     });
