94f4e1a6652edc3ac2e5f744be3d16a0c3b3f8af
dimik <dimik@yandex.ru>
2013-08-23 17:05:23 +0700

    extend options

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Tiler = require('./lib/layer-tiler.js'),
     destination = __dirname + '/images';
 
-var tiler = new Tiler();
+var tiler = new Tiler({ output: destination });
 
 tiler.openSource(destination + '/big.jpg')
     .then(function () {
