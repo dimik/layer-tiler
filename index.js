b85ffff0d0c29cb1a0f42d47597e8f659a4143aa
dimik <dimik@yandex.ru>
2013-11-15 15:42:30 +0700

    ignore tests

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-var Tiler = require('./lib/layer-tiler.js'),
-    destination = __dirname + '/images',
-    start = new Date;
-
-var tiler = new Tiler({ output: destination });
-
-// tiler.openSource(destination + '/cosmo_map_final.png')
-tiler.openSource(destination + '/big.jpg')
-    .then(function () {
-        tiler.render()
-            .then(function () {
-                console.log('Rendered in %ds', Math.ceil((new Date - start) / 1000));
-            });
-    });
+module.exports = require('./lib/layer-tiler');
